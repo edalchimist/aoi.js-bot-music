299dd2e88c5c949ad11f284792e3ded7ccb3945a
edalchimist <109868232+edalchimist@users.noreply.github.com>
2022-10-05 14:29:41 +0100

    Update stop.js

--- commandes/stop.js
+++ commandes/stop.js
@@ -2,6 +2,5 @@ module.exports = [{
     name:"stop",
     code: `$title[stop]
 $description[Musique arrétée !]
-$image[https://www.bing.com/images/search?view=detailV2&ccid=V0QZcGb0&id=2253876D8F9A1C408857CB224E8A4E78F75ABDF7&thid=OIP.V0QZcGb067x8-QVlApwpdQHaHa&mediaurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.5744197066f4ebbc7cf90565029c2975%3frik%3d971a93hOik4iyw%26riu%3dhttp%253a%252f%252fcdn.onlinewebfonts.com%252fsvg%252fimg_339468.png%26ehk%3dszBWgJsqJz9p%252flVViYjvywb2tNwOtns1jzG9LIOVU5c%253d%26risl%3d%26pid%3dImgRaw%26r%3d0&exph=980&expw=980&q=stop+music&simid=608054111324741529&FORM=IRPRST&ck=EEDF6B5005B913EB5D0513E84B9E9E29&selectedIndex=2]
 $stop`
     }]
